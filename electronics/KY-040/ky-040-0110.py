3f20f0328e63596024ac2d88ea080933997cc512
qbytes-dq <qbytes.dq@gmail.com>
2016-09-18 20:53:53 -0400

    Adding Rotary Encoder
    Signed-off-by: qbytes-dq <qbytes.dq@gmail.com>

---
+++ electronics/KY-040/ky-040-0110.py
@@ -0,0 +1,98 @@
+# External module imports
+import RPi.GPIO as GPIO
+import time
+
+val=0
+
+# Pin Definitons:
+
+aPin = 20
+bPin = 21
+
+cPin = 16
+
+lastA = 0
+lastB = 0
+
+
+
+def encoderswitch(pin):
+    print ('switch', pin)
+
+def encodercount(pin):
+    global val
+    global lastA
+    global lastB
+    
+    dir=GPIO.input(bPin)
+    print pin, dir
+
+    if pin==aPin:
+        lastA=dir
+
+    if pin==bPin:
+        lastB=dir
+
+    if lastA==1 and lastB == 1:
+            print "--"
+    
+#    if GPIO.input(bPin)==0:
+#      dir="U"
+#      if val<20:
+#        val+=1
+#      #print "inc"
+#    else:
+#      dir="D"
+#      if val>0:
+#        val-=1
+#        #print "dec"
+
+#    print dir, val
+
+# Pin Definitons:
+#cPin = 16 
+#aPin = 20
+#bPin = 21
+
+# Pin Setup:
+GPIO.setmode(GPIO.BCM) # Broadcom pin-numbering scheme
+
+GPIO.setup(aPin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # 
+GPIO.setup(bPin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) #
+
+GPIO.setup(cPin, GPIO.IN, pull_up_down=GPIO.PUD_UP) # 
+
+# Initialize the interrupts - these trigger on the both the rising and falling 
+GPIO.add_event_detect(cPin, GPIO.FALLING, callback = encoderswitch, bouncetime=250)   # Encoder Button
+
+#GPIO.add_event_detect(aPin, GPIO.RISING, callback = encodercount, bouncetime=5)   # Encoder A
+#GPIO.add_event_detect(aPin, GPIO.RISING, callback = encodercount)   # Encoder A
+
+GPIO.add_event_detect(aPin, GPIO.BOTH, callback = encodercount)   # Encoder A
+GPIO.add_event_detect(bPin, GPIO.BOTH, callback = encodercount)   # Encoder B
+
+hist = "2222"
+print "1) ",hist
+hist += "a"
+hist = hist[1:5]
+print "2) ",hist
+hist += "b"
+hist = hist[1:5]
+print "2) ",hist
+hist += "c"
+hist = hist[1:5]
+print "2) ",hist
+hist += "d"
+hist = hist[1:5]
+print "2) ",hist
+
+
+print ("Here we go! Press CTRL+C to exit")
+try:
+    while 1:
+        time.sleep(0.01)
+
+except KeyboardInterrupt: # If CTRL+C is pressed, exit cleanly:
+    GPIO.remove_event_detect(aPin) # remove event
+    GPIO.remove_event_detect(cPin) # remove event
+    GPIO.cleanup() # cleanup all GPIO
---
+++ electronics/KY-040/ky-040.py
@@ -0,0 +1,63 @@
+# External module imports
+import RPi.GPIO as GPIO
+import time
+
+val=0
+
+# Pin Definitons:
+
+aPin = 20 # clock
+bPin = 21 # data/direction
+
+cPin = 16
+
+def encoderswitch(pin):
+    print ('switch', pin)
+
+def encodercount(pin):
+    global val
+    
+##    dir=GPIO.input(bPin)
+
+    if GPIO.input(bPin)==0:
+      dir="U"
+      if val<30:
+        val+=1
+      #print "dec"
+    else:
+      dir="D"
+      if val>00:
+        val-=1
+        #print "inc"
+
+    print dir, val
+
+# Pin Definitons:
+#cPin = 16 
+#aPin = 20
+#bPin = 21
+
+# Pin Setup:
+GPIO.setmode(GPIO.BCM) # Broadcom pin-numbering scheme
+
+GPIO.setup(cPin, GPIO.IN, pull_up_down=GPIO.PUD_UP) # 
+GPIO.setup(aPin, GPIO.IN, pull_up_down=GPIO.PUD_UP) # 
+GPIO.setup(bPin, GPIO.IN, pull_up_down=GPIO.PUD_UP) #
+
+# Initialize the interrupts - these trigger on the both the rising and falling 
+GPIO.add_event_detect(cPin, GPIO.FALLING, callback = encoderswitch, bouncetime=50)   # Encoder A
+
+#GPIO.add_event_detect(aPin, GPIO.RISING, callback = encodercount, bouncetime=5)   # Encoder A
+#GPIO.add_event_detect(aPin, GPIO.RISING, callback = encodercount)   # Encoder A
+GPIO.add_event_detect(aPin, GPIO.RISING, callback = encodercount)   # Encoder A
+#GPIO.add_event_detect(bPin, GPIO.BOTH, callback = encodercount)   # Encoder B
+
+print ("Here we go! Press CTRL+C to exit")
+try:
+    while 1:
+        time.sleep(0.01)
+
+except KeyboardInterrupt: # If CTRL+C is pressed, exit cleanly:
+    GPIO.remove_event_detect(aPin) # remove event
+    GPIO.remove_event_detect(cPin) # remove event
+    GPIO.cleanup() # cleanup all GPIO
